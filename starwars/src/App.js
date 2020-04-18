4ad28e52a60234cbbb5d04e23e32602fd2a3654b
Mac <xuromeo@gmail.com>
2020-04-17 21:21:14 -0700

    4/17

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import './App.css';
-
+import axios from 'axios';
+import Character from './components/Character'
+// import { Container, Row,Col} from 'reactstrap';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -8,11 +10,26 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+const [data,setData] = useState([]);
 
+  useEffect(()=>{
+  const fetchData = async ()=>{
+    const result = await axios("https://rickandmortyapi.com/api/character/"
+      );
+      console.log(result.data.results);
+      setData(result.data.results)
+  };
+  fetchData();
+},[]);
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+  <div>
+    <h1>characters</h1>
+    <div className='box'>
+      
+      {data.map(item=>(<Character item={item} key={item.id}/>))}
+      
     </div>
+  </div>
   );
 }
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, 
+  } from 'reactstrap';
+
+export default function Character(props) {
+    return (
+   
+        <Card className='card'>
+          <CardImg top width="100%" src={props.item.image} alt="Card image cap" />
+          <CardBody>
+            <CardTitle>{props.item.name}</CardTitle>
+            <CardSubtitle>{props.item.species}</CardSubtitle>
+            <CardText>{props.item.status}</CardText>
+           
+          </CardBody>
+        </Card>
+    
+        
+    )
+}
